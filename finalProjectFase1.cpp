ef1b18f5358f2c925a1585c2ac40eb85ee68fcc5
M. Hikari Reiziq Rakhadinta <reiziqzip@gmail.com>
2026-01-04 22:11:39 +0700

    Final Mini Project Fase 1

--- finalProjectFase1.cpp
+++ finalProjectFase1.cpp
@@ -11,20 +11,60 @@ struct Produk{
 // 1. Roti - Rp 5000
 // 2. Susu - Rp 10000
 
-
-
-
 int main(){
     
-    Produk produk1;
-    Produk produk2;
+    Produk barang[2];
     
-    Produk produk1.nama = "Roti";
-    Produk produk1.harga = 5000;
-    
-    
-    Produk produk2.nama = "Susu";
-    Produk produk2.harga = 10000;
+    barang[0].nama = "roti";
+    barang[0].harga = 5000;
 
+    barang[1].nama = "susu";
+    barang[1].harga = 10000;
+    
+    int pilihan;
+    int beli;
+    int total;
+    int bayar;
+    int kembalian;
+    
+    cout << "=== MENU WARUNG ===" << endl;
+    for (int i = 0; i < 2; i++){
+        cout << i+1 << "." << barang[i].nama << " - Rp " << barang[i].harga << endl;
+    };
+    
+    cout << "Pilih nomor barang: ";
+    cin >> pilihan;
 
+    while ( pilihan != 1 && pilihan != 2){
+    cout << "Pilih nomor barang kembali: ";
+    cin >> pilihan;     
+    }
+
+    switch (pilihan){
+        case 1:
+        
+        cout << "Jumlah beli: ";
+        cin >> beli;       
+        total = barang[0].harga * beli;
+        cout << "Total harga: " << total << endl;
+        break;
+        case 2:
+
+        cout << "Jumlah beli: ";
+        cin >> beli;
+        total = barang[1].harga * beli;
+        cout << "Total harga: " << total << endl;
+        break;
+        default:
+        cout << "Pilihan produk tidak tersedia" << endl;
+    }
+    
+    cout << "Bayar: ";
+    cin >> bayar;
+
+    kembalian = bayar - total; 
+    
+    cout << "Kembalian: " << kembalian << endl << "Terima kasih!" << endl;
+
+    return 0;
 }
