cfaf7d8e5379c2c822df0adb82d6c1aa0066ecfd
M. Hikari Reiziq Rakhadinta <reiziqzip@gmail.com>
2026-01-15 15:19:08 +0700

    Final Mini Project Fase 3

--- Fase 3 (OOP)/Finalproject.cpp
+++ Fase 3 (OOP)/Finalproject.cpp
@@ -15,30 +15,83 @@ using namespace std;
 // Masukkan Jurusan: Teknik Informatika
 // [Info] Data berhasil disimpan!
 
-class Mahasiswa {
-    public: 
-    string nama;
-    int nim;
-    string jurusan;
 
-    void tambah(){
+
+// === DATA MAHASISWA ===
+// 1. Nama: Hikar | NIM: 2024001 | Jurusan: Teknik Informatika
+// -------------------------
+
+
+
+// Masukkan NIM yang dicari: 2024001
+
+// [DITEMUKAN!]
+// Nama   : Hikar
+// Jurusan: Teknik Informatika
+
+class Mahasiswa {
+    public:
+    string nama;
+    string nim;
+    string jurusan;
+    string baris;
+    string cariNim;
+    int nomor = 1;
+
+
+    void tambahMahasiswa(){
+        ofstream database ("mahasiswa.txt", ios::app);
         cout << "Masukkan Nama   :";
         cin >> nama;
+        cin.ignore();
         cout << "Masukkan NIM    :";
         cin >> nim;
+        cin.ignore();
         cout << "Masukkan Jurusan:";
-        cin >> jurusan;
+        getline(cin, jurusan);
+        database << "Nama: " << nama << " | " << "NIM: "  << nim << " | " << "Jurusan: " << jurusan << endl;
+        cout << "\n[Info] Data berhasil disimpan!\n\n";
+        database.close();
+    }
+
+    void lihatData(){
+        ifstream readDatabase("mahasiswa.txt");
+        cout << "=== DATA MAHASISWA ===\n";
+        while (getline (readDatabase, baris)){
+            cout << nomor << ". " << baris << endl;
+            nomor++;
+        }
+        cout << endl;
+        nomor = 1;
+    }
+    
+    void cariData(){
+        bool ketemu =  false;
+        cout << "Masukkan NIM yang dicari: ";
+        cin >> cariNim;
+        ifstream readDatabase("mahasiswa.txt");
+        while (getline (readDatabase, baris)){
+            if (baris.find(cariNim) != string::npos){
+                cout << "[DITEMUKAN!]" << endl;
+                cout << baris << endl;
+                ketemu = true;
+            }
+        }
+
+        if (ketemu != true){
+            cout << "[Data tidak ditemukan]";
+        }
+        cout << endl;
     }
 
 
-};
+   };
 
 int main (){
-
     Mahasiswa mhs;
-
+    
     int choice;
-
+    
     do {
         cout << "=== MENU KAMPUS ===\n";
         cout << "1. Tambah Mahasiswa\n";
@@ -47,17 +100,26 @@ int main (){
         cout << "4. Keluar\n";
         cout << "Pilihan: ";
         cin >> choice;
-
+        
         switch (choice)
         {
             case 1:
-                mhs.tambah();
+                // Mahasiswa mhs(nama, nim, jurusan);
+                mhs.tambahMahasiswa();
                 break;
             case 2:
+                mhs.lihatData();
+                break;
+            case 3:
+                mhs.cariData();
+                break;
+            case 4:
+                cout << "[Exiting program. Goodbye!]\n";
+                break;
 
             default:
-            cout << "Invalid option!\n"
+            cout << "Invalid option!\n";
             
         }
-    } while (!choice);
+    } while (choice != 4);
 }
--- Fase 3 (OOP)/tempCodeRunnerFile.cpp
+++ Fase 3 (OOP)/tempCodeRunnerFile.cpp
@@ -1,21 +1,100 @@
 #include <iostream>
+#include <string>
 #include <fstream>
 using namespace std;
 
-// Siapa nama anda: Hikar
-// Tulis quote hari ini: Belajar C++ butuh kesabaran.
-int main (){
-    ofstream diary ("diary.txt");
+// === MENU KAMPUS ===
+// 1. Tambah Mahasiswa
+// 2. Lihat Data
+// 3. Cari Mahasiswa
+// 4. Keluar
+// Pilihan: 1
+
+// Masukkan Nama   : Hikar
+// Masukkan NIM    : 2024001
+// Masukkan Jurusan: Teknik Informatika
+// [Info] Data berhasil disimpan!
+
+
+
+// === DATA MAHASISWA ===
+// 1. Nama: Hikar | NIM: 2024001 | Jurusan: Teknik Informatika
+// -------------------------
+
+
+
+// Masukkan NIM yang dicari: 2024001
+
+// [DITEMUKAN!]
+// Nama   : Hikar
+// Jurusan: Teknik Informatika
+
+class Mahasiswa {
+    public:
     string nama;
-    string quote;
-    cout << "Siapa nama anda: ";
-    cin >> nama;
-    cin.ignore();
-    cout << "Tulis quote hari ini: ";
-    getline(diary, quote);
+    int nim;
+    string jurusan;
+    string baris;
 
-    diary.close();
-    cout << "[Sistem] Data sedang disimpan ke diary.txt...\n";
-    cout << "[Sistem] Membaca isi file diary.txt... {coming soon}\n";
 
+    void tambahMahasiswa(){
+        ofstream database ("mahasiswa.txt");
+        cout << "Masukkan Nama   :";
+        cin >> nama;
+        cout << "Masukkan NIM    :";
+        cin >> nim;
+        cin.ignore();
+        cout << "Masukkan Jurusan:";
+        getline(cin, jurusan);
+        cout << "[Info] Data berhasil disimpan!\n";
+        database << "1. Nama: " << nama << " | " << "NIM: "  << nim << " | " << "Jurusan: " << jurusan << endl;
+        database.close();
+    }
+
+    void lihatData(){
+        ifstream readDatabase("mahasiswa.txt");
+        cout << "=== DATA MAHASISWA ===\n";
+        while (getline (readDatabase, baris)){
+        cout << baris << endl;
+        }
+    }
+
+
+};
+
+int main (){
+    Mahasiswa mhs;
+    
+    int choice;
+    
+    do {
+        cout << "=== MENU KAMPUS ===\n";
+        cout << "1. Tambah Mahasiswa\n";
+        cout << "2. Lihat Data\n";
+        cout << "3. Cari Mahasiswa\n";
+        cout << "4. Keluar\n";
+        cout << "Pilihan: ";
+        cin >> choice;
+        
+        switch (choice)
+        {
+            case 1:
+                // Mahasiswa mhs(nama, nim, jurusan);
+                mhs.tambahMahasiswa();
+                break;
+            case 2:
+                mhs.lihatData();
+                break;
+            // case 3:
+            //     mhs.cariData();
+            //     break;
+            case 3:
+                cout << "Exiting program. Goodbye!\\n";
+                break;
+
+            default:
+            cout << "Invalid option!\n";
+            
+        }
+    } while (choice != 4);
 }
